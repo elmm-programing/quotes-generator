682dbbfe0b5e7f047d6c71e04b84b7df419bae0b
ELMM <elmm.programing@gmail.com>
2024-03-28 15:12:52 -0400

    Dom manipulation added to refresh quote text and event listener add in button newQuote to get another quoute

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
 // Get quotes from api
 let apiQuotes = []
 
-const newQuote = ()=>{
+const quoteText = document.getElementById('quote')
+const authorText = document.getElementById('author')
+const buttonNewQuote = document.getElementById('new-quote')
+
+buttonNewQuote.addEventListener('click', () => {
+  newQuote()
+})
+
+const newQuote = () => {
   if (apiQuotes.length == 0) {
     getQuotes()
   }
-const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)]
-  console.log(quote)
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   
+  quoteText.innerText = quote.text ? quote.text:"Unknown" 
+  authorText.innerText = quote.author ? quote.author:"Unknown"
+  console.log(quote)
+
 }
 
-const getQuotes = async ()=>{
-const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json"
+const getQuotes = async () => {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json"
   try {
-    const response = await fetch(apiUrl); 
+    const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote()
     console.log(apiQuotes)
@@ -22,3 +33,6 @@ const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json"
   }
 }
 getQuotes()
+
+
+
