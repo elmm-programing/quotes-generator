355916f7a107e27412b7036c9b121fc48147f504
ELMM <elmm.programing@gmail.com>
2024-03-28 15:00:29 -0400

    Fetching quotes added and Get random quote functions added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+// Get quotes from api
+let apiQuotes = []
+
+const newQuote = ()=>{
+  if (apiQuotes.length == 0) {
+    getQuotes()
+  }
+const quote = apiQuotes[Math.floor(Math.random()* apiQuotes.length)]
+  console.log(quote)
+  
+}
+
+const getQuotes = async ()=>{
+const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json"
+  try {
+    const response = await fetch(apiUrl); 
+    apiQuotes = await response.json();
+    newQuote()
+    console.log(apiQuotes)
+  } catch (err) {
+    console.log(err)
+  }
+}
+getQuotes()
